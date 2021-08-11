11c3328f63d3c53b07187396480452f6f1cfda9c
benrogue6 <84081641+benrogue6@users.noreply.github.com>
2021-08-11 16:32:10 +0300

    Add files via upload

---
+++ maker/src/green.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+
+import sys
+import numpy as np
+#import rospy
+#import cv2
+import rospy, cv2, cv_bridge, numpy
+from std_msgs.msg import String
+#from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+import time
+
+class image_converter:
+
+  def __init__(self):
+    self.cmd_vel_pub = rospy.Publisher("/cmd_vel",Twist, queue_size=2)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image, self.callback)
+    self.twist = Twist()
+
+   
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+     
+      #converting bgr to hsv in order to identify the green color
+      hsv_cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+   
+    except CvBridgeError as e:
+      print(e)
+
+  
+    lower_green = np.array([45, 100, 50])
+    upper_green = np.array([70, 255, 255]) 
+
+    masking = cv2.inRange(hsv_cv_image, lower_green, upper_green)
+   #M = cv2.moments(masking)
+    
+    cv2.imshow("Green_detection", masking)
+
+    cv2.waitKey(3)
+
+ 
+     
+   
+def main(args):
+  ic = image_converter()
+
+  rospy.init_node('traffic', anonymous = True)
+
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+    
---
+++ maker/src/light_dark.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import rospy, cv2, cv_bridge, numpy
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import Twist
+
+
+class dodge_cube:
+  def __init__(self):
+    self.bridge = cv_bridge.CvBridge()
+    
+    self.image_sub = rospy.Subscriber('/camera/color/image_raw',Image, self.image_callback)
+
+    self.cmd_vel_pub = rospy.Publisher('/cmd_vel',Twist, queue_size=1)
+
+    self.twist = Twist()
+
+  def image_callback(self, msg):
+    image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    lower_black = numpy.array([ 0, 0, 0])
+    upper_black = numpy.array([180, 255, 30])
+
+    lower_white = numpy.array([0,0,180])
+    upper_white = numpy.array([255,255,255])
+
+    mask1 = cv2.inRange(hsv, lower_black, upper_black)
+    mask2 = cv2.inRange(hsv, lower_white, upper_white)
+    
+    h, w, d = image.shape
+    search_top = 3*h/4
+    search_bot = 3*h/4 + 20
+ 
+    mask1[0:search_top, 0:w] = 0
+    mask1[search_bot:h, 0:w] = 0
+    mask2[0:search_top, 0:w] = 0
+    mask2[search_bot:h, 0:w] = 0
+
+    M1 = cv2.moments(mask1)
+    M2 = cv2.moments(mask2)
+
+    if M2['m00'] > 0:
+      cx2 = int(M2['m10']/M2['m00'])
+      cy2 = int(M2['m01']/M2['m00'])
+      err = cx2 - w/2
+      
+      self.twist.linear.x = 0.2
+      self.twist.angular.z = -float(err) / 100
+      self.cmd_vel_pub.publish(self.twist)
+
+    elif M1['m00'] > 0:
+      cx1 = int(M1['m10']/M1['m00'])
+      cy1 = int(M1['m01']/M1['m00'])
+      err = cx1 - w/2
+
+      #track for change in the x position of black mask blob
+      if cx1 > 400:
+        self.twist.angular.z = -float(err) / 100
+        self.cmd_vel_pub.publish(self.twist)
+
+      elif cx1 < 250:
+        self.twist.angular.z = float(err) / 100
+
+        self.twist.linear.x = 0.7
+        self.cmd_vel_pub.publish(self.twist)
+      
+    #cv2.imshow("white_mask",mask2)
+    cv2.imshow("black_mask",mask1)
+    cv2.imshow("Colored_Image", image)
+    cv2.imshow("Special_Image", gray_image)
+
+    cv2.waitKey(3)
+
+rospy.init_node('cube_node')
+dodge_cube = dodge_cube()
+rospy.spin()
